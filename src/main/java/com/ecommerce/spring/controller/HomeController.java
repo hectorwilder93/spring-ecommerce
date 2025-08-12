b93ce66fa57f84bd9f9b10fcc5aaccd798829c8d
hectorwilder93 <cocorotero@hotmail.com>
2025-08-12 09:51:52 -0500

    Creamos como mostrar la información del productohome

--- src/main/java/com/ecommerce/spring/controller/HomeController.java
+++ src/main/java/com/ecommerce/spring/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.ecommerce.spring.controller;
 
+
+import com.ecommerce.spring.model.Producto;
 import com.ecommerce.spring.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+import static org.slf4j.Logger.*;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final Logger log=LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -18,5 +28,17 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model){
+        log.info("Id producto enviado como parámetro {}",id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+        model.addAttribute("producto", producto);
+
+
+        return "usuario/productohome";
     }
 }
