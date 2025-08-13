307e1d09c7b10e8a5ca34b849a63dee0919459cf
hectorwilder93 <cocorotero@hotmail.com>
2025-08-13 16:59:48 -0500

    Creamos que el usuario pueda ver el detalle de la compra

--- src/main/java/com/ecommerce/spring/service/IOrdenService.java
+++ src/main/java/com/ecommerce/spring/service/IOrdenService.java
@@ -4,9 +4,11 @@ import com.ecommerce.spring.model.Orden;
 import com.ecommerce.spring.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
     List<Orden> findAll();
+    Optional<Orden> findById(Integer id);
     Orden save(Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario(Usuario usuario);
